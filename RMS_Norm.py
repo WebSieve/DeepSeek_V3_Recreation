782801889f5eb5440f73c73f73852d0f1701f4c0
Sahil Murmu <msahil2603@gmail.com>
2025-12-27 18:22:49 +0000

    Root Mean Square Normalization

---
+++ RMS_Norm.py
@@ -0,0 +1,16 @@
+import torch.nn as nn
+import torch
+
+
+class RMSNorm(nn.Module):
+    """Root Mean Square Layer Normalization"""
+
+    def __init__(self, hidden_size: int, eps: float = 1e-6):
+        super().__init__()
+        self.weight = nn.Parameter(torch.ones(hidden_size))
+        self.eps = eps
+
+    def forward(self, hidden_states):
+        variance = hidden_states.pow(2).mean(-1, keepdim=True)
+        hidden_states = hidden_states * torch.rsqrt(variance + self.eps)
+        return self.weight * hidden_states
